900fea7a4153ced7c54dcd91d977f6425df6213e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-04-16 23:17:54 +0100

    Calculate positions for strings & positions, draw highlighted positions

--- main.lua
+++ main.lua
@@ -1,3 +1,10 @@
+local stringPositionNotes = {
+    { 'E', 'F#', 'G#', 'A', 'B' },
+    { 'A', 'B', 'C#', 'D', 'E' },
+    { 'D', 'E', 'F#', 'G', 'A' },
+    { 'G', 'A', 'B', 'C', 'D' }
+}
+
 function love.load()
 
 end
@@ -7,7 +14,7 @@ function love.update(dt)
 end
 
 function love.draw()
-    drawFingerboard(10, 10, 675, 60, 90)
+    drawFingerboard(50, 50, 675, 60, 90)
 end
 
 function love.keypressed(key)
@@ -18,40 +25,51 @@ end
 
 function drawFingerboard(x, y, length, headWidth, bodyWidth)
     love.graphics.setBackgroundColor(0.7, 0.7, 0.5)
-    local position1, position2, position3, position4 = 3.5 / 22.5, 7 / 22.5, 9 / 22.5, 12 / 22.5
-    local height1, height2, height3, height4 = 
-        ((bodyWidth - headWidth) / 2) * (1 - position1),
-        ((bodyWidth - headWidth) / 2) * (1 - position2),
-        ((bodyWidth - headWidth) / 2) * (1 - position3),
-        ((bodyWidth - headWidth) / 2) * (1 - position4),
-    love.graphics.setColor(0.1, 0.1, 0.1)
+    local positions = { 0, 3.5 / 22.5, 7 / 22.5, 9 / 22.5, 12 / 22.5 }
+    local heights = {}
+
+    for i = 1, 5 do
+        table.insert(heights, ((bodyWidth - headWidth) / 2) * (1 - positions[i]))
+    end
+
+    love.graphics.setColor(0.2, 0.2, 0.1)
     love.graphics.setLineWidth(2)
     love.graphics.polygon('line', x, y + (bodyWidth - headWidth) /  2, x + length, y, x + length, y + bodyWidth, x, y + headWidth + (bodyWidth - headWidth) / 2)
     love.graphics.polygon('fill', x, y + (bodyWidth - headWidth) /  2, x + length, y, x + length, y + bodyWidth, x, y + headWidth + (bodyWidth - headWidth) / 2)
-    love.graphics.setColor(0.5, 0.5, 0.5)
-    love.graphics.line(x + position1 * length, y + height1, x + position1 * length, y + bodyWidth - height1)
-    love.graphics.line(x + position2 * length, y + height2, x + position2 * length, y + bodyWidth - height2)
-    love.graphics.line(x + position3 * length, y + height3, x + position3 * length, y + bodyWidth - height3)
-    love.graphics.line(x + position4 * length, y + height4, x + position4 * length, y + bodyWidth - height4)
 
-    love.graphics.setColor(0.75, 0.75, 0.75)
-    love.graphics.circle('fill', x + position1 * length, y + (bodyWidth * 1) / 5, 5)
-    love.graphics.circle('fill', x + position1 * length, y + (bodyWidth * 2) / 5, 5)
-    love.graphics.circle('fill', x + position1 * length, y + (bodyWidth * 3) / 5, 5)
-    love.graphics.circle('fill', x + position1 * length, y + (bodyWidth * 4) / 5, 5)
+    love.graphics.setColor(1, 1, 1)
+    for i = 1, 5 do
+        love.graphics.line(x + positions[i] * length, y + heights[i], x + positions[i] * length, y + bodyWidth - heights[i])
+    end
 
-    love.graphics.circle('fill', x + position2 * length, y + (bodyWidth * 1) / 5, 5)
-    love.graphics.circle('fill', x + position2 * length, y + (bodyWidth * 2) / 5, 5)
-    love.graphics.circle('fill', x + position2 * length, y + (bodyWidth * 3) / 5, 5)
-    love.graphics.circle('fill', x + position2 * length, y + (bodyWidth * 4) / 5, 5)
+    love.graphics.setColor(0.8, 0.8, 0.8)
+    for i = 1, 4 do
+        love.graphics.line(x, y + ((bodyWidth - headWidth) / 2) + headWidth * (i / 4) - headWidth / 8, x + length, y + bodyWidth * (i / 4) - bodyWidth / 8)
+    end
+    
+    local highlightedNote = ''
+    for i = 1, 5 do
+        for j = 1, 4 do
+            local pointX = x + positions[i] * length
+            -- local pointY = y + (bodyWidth * j) / 4 - bodyWidth / 8
+            local height = headWidth + (bodyWidth - headWidth) * positions[i]
+            local offset = ((bodyWidth - headWidth) / 2) * (1 - positions[i])
+            local pointY = y + (height * j / 4) + offset - height / 8
+            local mouseX, mouseY = love.mouse.getPosition()
+            local radius = 7.5
+            if math.sqrt(math.pow(pointX - mouseX, 2) + math.pow(pointY - mouseY, 2)) < radius then
+                love.graphics.setColor(1, 0, 0)
+                highlightedNote = stringPositionNotes[j][i]
+            else
+                love.graphics.setColor(0.75, 0.75, 0.75)
+            end                
+            love.graphics.circle('fill', pointX, pointY, radius)
+            love.graphics.setColor(0, 0, 0)
+            love.graphics.setLineWidth(1)
+            love.graphics.circle('line', pointX, pointY, radius)
+        end
+    end
 
-    love.graphics.circle('fill', x + position3 * length, y + (bodyWidth * 1) / 5, 5)
-    love.graphics.circle('fill', x + position3 * length, y + (bodyWidth * 2) / 5, 5)
-    love.graphics.circle('fill', x + position3 * length, y + (bodyWidth * 3) / 5, 5)
-    love.graphics.circle('fill', x + position3 * length, y + (bodyWidth * 4) / 5, 5)
-
-    love.graphics.circle('fill', x + position4 * length, y + (bodyWidth * 1) / 5, 5)
-    love.graphics.circle('fill', x + position4 * length, y + (bodyWidth * 2) / 5, 5)
-    love.graphics.circle('fill', x + position4 * length, y + (bodyWidth * 3) / 5, 5)
-    love.graphics.circle('fill', x + position4 * length, y + (bodyWidth * 4) / 5, 5)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print(highlightedNote, 20, 85)
 end
